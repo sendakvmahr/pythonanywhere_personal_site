e8dc13e0ec60c194bad6a5454d11debc1827dd27
vivianmahr <vivianmahr@gmail.com>
2020-02-23 17:28:02 -0800

    more text, links

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,23 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
+def homepage():
+    return render_template("index.html")
+
+@app.route('/web_design')
+def web_design():
+    return render_template("index.html", title="Home")
+
+@app.route('/development')
+def development():
+    return render_template("index.html", title="Home")
+
+@app.route('/pixel_art')
+def pixel_art():
+    return render_template("index.html", title="Home")
+
+
+@app.route('/about')
+def about():
     return render_template("index.html", title="Home")
 
